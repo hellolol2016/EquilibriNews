c89f05093cb62fc58d242cfab3d47d363bee11c6
hellolol2016 <denniswang812@gmail.com>
2022-05-29 15:36:25 -0700

    index formatting

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,12 +8,24 @@ import {
   MediaQuery,
   Burger,
   useMantineTheme,
-} from '@mantine/core';
+  Stack,
+  Space,
+  Box,
+} from "@mantine/core";
+import Link from "next/link";
 
 export default function AppShellDemo() {
-  return(
-    <Header height={100}>
-      <Text>Hello world</Text>
-    </Header>
-    ) 
+  return (
+    <Box>
+      <Box fixed sx={{ position:"absolute"}}>
+        <Link href="/" >
+          <Text>Insert Image Here Later</Text>
+        </Link>
+        </Box>
+      <Stack height={100} justify={"flex-end"} sx={{ flexDirection: "row" }}>
+        <Link href="about">About</Link>
+        <Link href="tutorial">Get Started</Link>
+      </Stack>
+    </Box>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Center, Stack } from "@mantine/core";
+import { Center, Footer, Grid, SimpleGrid, Stack, Text } from "@mantine/core";
 import Navbar from "../components/Navbar";
+import Link from "next/link";
 export default function Home() {
   return (
     <Stack>
       <Navbar> </Navbar>
       <Center>
+        <Stack align="center">
         <h1>EquilibriNews</h1>
+        <Text>Only facts.</Text>
+</Stack>
       </Center>
+      <Stack align="center">
+        <h2>Sources</h2>
+        <SimpleGrid></SimpleGrid>
+      </Stack>
+      <Footer >
+        <Stack justify="center" sx={{flexDirection:"row"}}>
+          <Link href="#"><Text>Instagram</Text></Link>
+          <Link href="#"><Text>Github</Text></Link>
+          <Link href="#"><Text>Youtube</Text></Link>
+        </Stack>
+      </Footer>
     </Stack>
   );
 }
