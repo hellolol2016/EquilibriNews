b3ae8270d609b0df7d698ac70e4086ce2299f073
Dennis Wang <66754085+hellolol2016@users.noreply.github.com>
2022-05-29 15:00:35 -0700

    staretd

---
+++ components/Navbar.js
@@ -0,0 +1,19 @@
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Footer,
+  Aside,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from '@mantine/core';
+
+export default function AppShellDemo() {
+  return(
+    <Header height={100}>
+      <Text>Hello world</Text>
+    </Header>
+    ) 
+}
