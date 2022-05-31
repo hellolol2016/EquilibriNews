4475eaf2ffcbdc7f81db72d4fee32925da8a8fb5
hellolol2016 <denniswang812@gmail.com>
2022-05-31 06:22:11 -0700

    more index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Center, Footer, Grid, SimpleGrid, Stack, Text } from "@mantine/core";
+import {
+  Button,
+  Center,
+  Footer,
+  Grid,
+  SimpleGrid,
+  Stack,
+  Text,
+} from "@mantine/core";
 import Navbar from "../components/Navbar";
 import Link from "next/link";
 export default function Home() {
@@ -9,19 +17,31 @@ export default function Home() {
       <Navbar> </Navbar>
       <Center>
         <Stack align="center">
-        <h1>EquilibriNews</h1>
-        <Text>Only facts.</Text>
-</Stack>
+          <h1>EquilibriNews</h1>
+          <Text>Only facts.</Text>
+        </Stack>
       </Center>
       <Stack align="center">
         <h2>Sources</h2>
         <SimpleGrid></SimpleGrid>
       </Stack>
-      <Footer >
-        <Stack justify="center" sx={{flexDirection:"row"}}>
-          <Link href="#"><Text>Instagram</Text></Link>
-          <Link href="#"><Text>Github</Text></Link>
-          <Link href="#"><Text>Youtube</Text></Link>
+      <Footer>
+        <Stack justify="center" sx={{ flexDirection: "row" }}>
+          <Link href="#">
+            <Button sx={{ paddingTop: "3px" }}>
+              <Text>Instagram</Text>
+            </Button>
+          </Link>
+          <Link href="#">
+            <Button sx={{ paddingTop: "3px" }}>
+              <Text>Github</Text>
+            </Button>
+          </Link>
+          <Link href="#">
+            <Button sx={{ paddingTop: "3px" }}>
+              <Text>Youtube</Text>
+            </Button>
+          </Link>
         </Stack>
       </Footer>
     </Stack>
