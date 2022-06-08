1e28d0f846ec2a29c450556ad73a466010f09356
Dennis Wang <66754085+hellolol2016@users.noreply.github.com>
2022-06-07 20:10:14 -0700

    day1

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import {
+  BackgroundImage,
   Box,
   Button,
   Center,
@@ -13,30 +14,41 @@ import Navbar from "../components/Navbar";
 import Link from "next/link";
 import Image from "next/image";
 export default function Home() {
+  const NewsSource = (props) => {
+    return (
+      <Box
+        sx={{ display: "flex", justifyContent: "center", alignSelf: "center" }}
+      >
+        <Image
+          src={`/media/${props.img}.png`}
+          alt={props.alt}
+          width={200}
+          height={props.h}
+        />
+      </Box>
+    );
+  };
+
   return (
     <Stack>
-      <Navbar> </Navbar>
       <Center>
-        <Stack align="center">
-          <h1>EquilibriNews</h1>
-          <Text>Only facts.</Text>
-        </Stack>
+        <BackgroundImage src="/media/background.jpg" opacity>
+          <Navbar />
+          <Stack align="center" sx={{ height: "800px" }}>
+            <Text fontSize={300}>EquilibriNews</Text>
+            <Text>Only facts.</Text>
+          </Stack>
+        </BackgroundImage>
       </Center>
       <Stack align="center">
         <h2>Sources</h2>
         <SimpleGrid cols={3}>
-          <Box>
-            <Image src="../media/abc.png"  width={200} height={200}/>
-            ABC News</Box>
-          <Box>Fox News</Box>
-          <Box>New York Times</Box>
-
-          <Box>ABC News</Box>
-          <Box>Fox News</Box>
-          <Box>New York Times</Box>
-
-
-
+          <NewsSource img="abc" alt={"abc news"} h={200} />
+          <NewsSource img="wsj" alt={"wall street journal"} h={200} />
+          <NewsSource img="nyt" alt={"new york times"} h={150} />
+          <NewsSource img="fox" alt={"fox news"} h={200} />
+          <NewsSource img="cnn" alt={"cnn"} h={200} />
+          <NewsSource img="dm" alt={"dm"} h={100} />
         </SimpleGrid>
       </Stack>
       <Footer>
