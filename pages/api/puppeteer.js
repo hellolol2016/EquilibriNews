1748ec36ced3282433d3e7f536ae73ac434e348c
Dennis Wang <66754085+hellolol2016@users.noreply.github.com>
2022-06-27 18:20:54 -0700

    more puppets

--- pages/api/puppeteer.js
+++ pages/api/puppeteer.js
@@ -192,8 +192,8 @@ async function scrapeInfiniteScrollItems(
 
 export default async function handler(req, res) {
   const browser = await puppeteer.launch({
-    args:["--no-sandbox"],
-    headless: false,
+    args:["--no-sandbox","--disable-setuid-sandbox"],
+    
   });
   const page = await browser.newPage();
   page.setJavaScriptEnabled(false);
