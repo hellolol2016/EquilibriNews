99bc718577970b41140374d9a2afd553bc0cc6ef
Dennis Wang <66754085+hellolol2016@users.noreply.github.com>
2022-07-10 10:43:39 -0700

    trying to add more jsons

--- components/ArticleContainer.js
+++ components/ArticleContainer.js
@@ -1,25 +1,29 @@
 import { Box, SimpleGrid } from "@mantine/core";
 import Article from "./Article";
 
-export default function ArticleContainer({props, rating}) {
-  const articles  = data.articles
-  return <SimpleGrid cols={3}>{
-      articles.map((article)=>{
+export default function ArticleContainer({ props, rating }) {
+  return (
+    <SimpleGrid cols={3}>
+      {
+        articles.map((article)=>{
         return(
-          <Article key={article.title} title={article.title} type={article.type} />
-
-
+        <Article key={article.title} title={article.title} type={article.type} />
         )
-      })
-    
-    }</SimpleGrid>;
+        })
+      }
+    </SimpleGrid>
+  );
 }
 
-import data from "../public/articles/dm.json";
-
+import dm from "../public/articles/dm.json";
+import abc from "../public/articles/abc.json";
+import fox from "../public/articles/fox.json";
+import nyt from "../public/articles/nyt.json";
+import r from "../public/articles/r.json";
+import wsj from "../public/articles/wsj.json";
 export async function getStaticProps() {
   const rating = localStorage.getItem("rating");
   return {
-    props: {data},
+    props: {dm}
   };
 }
