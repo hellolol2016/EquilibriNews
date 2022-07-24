63c951cc2d2045adf61f509451264d55ec03677c
Dennis Wang <66754085+hellolol2016@users.noreply.github.com>
2022-07-24 16:27:00 -0700

    ehh sort??

--- components/ArticleContainer.js
+++ components/ArticleContainer.js
@@ -3,9 +3,45 @@ import Article from "./Article";
 
 
 export default function ArticleContainer({props, rating}) {
-  const articles  = abc.articles
+  const barticles  = abc.articles
+  let farticle = {};
+  if (rating >= 0) {
+    if (rating < 2) {
+      console.log("far left");
+      farticle.push(r.articles) 
+      farticle.push(fox.articles)
+      farticle.push(dm.articles)
+      farticle.push(wsj.articles)
+    } else if (rating < 4) {
+      console.log("mid left");
+      farticle.push(r.articles) 
+      farticle.push(fox.articles)
+      farticle.push(dm.articles)
+      farticle.push(wsj.articles)
+    } else if (rating < 7) {
+      console.log("neutral");
+      farticle.push(r.articles) 
+      farticle.push(wsj.articles)
+      farticle.push(r.articles) 
+      farticle.push(nyt.articles) 
+    } else if (rating < 9) {
+      console.log("mid right");
+      farticle.push(wsj.articles)
+      farticle.push(abc.articles)
+      farticle.push(r.articles) 
+      farticle.push(nyt.articles) 
+    } else if (rating < 11) {
+      console.log("far right");
+      farticle.push(nyt.articles) 
+      farticle.push(wsj.articles)
+      farticle.push(abc.articles)
+    } else {
+      console.log("errror");
+    }
+  }
+
   return (<SimpleGrid cols={3}>{
-      articles.map((article)=>{
+      barticles.map((article)=>{
         return(
         <Article key={article.title} title={article.title} type={article.type} />
         )
