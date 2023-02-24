9c273ef2506f5f8a7de26d23c31eba967da5b008
Dennis Wang <66754085+hellolol2016@users.noreply.github.com>
2023-02-23 18:33:57 -0800

    adding to list works yay

--- components/Gallery.js
+++ components/Gallery.js
@@ -19,75 +19,90 @@ import { useState, useEffect } from "react";
 import { Bars } from "react-loading-icons";
 
 const text_truncate = (str, length, ending) => {
-    if (length == null) {
-      length = 100;
-    }
-    if (ending == null) {
-      ending = '...';
-    }
-    if (str.length > length) {
-      return str.substring(0, length - ending.length) + ending;
-    } else {
-      return str;
-    }
-  };
-  
-const addToList = ( title, link, source,type ) => {
-    console.log("added " + title);
+  if (length == null) {
+    length = 100;
   }
+  if (ending == null) {
+    ending = "...";
+  }
+  if (str.length > length) {
+    return str.substring(0, length - ending.length) + ending;
+  } else {
+    return str;
+  }
+};
 
+const addToList = (title, link, source, type) => {
+  console.log("added " + title);
+  let thing = { title: title, type: type, url: link, source: source };
+  if (localStorage.getItem("list") !== null) {
+    console.log(typeof JSON.parse(localStorage.getItem("list")));
+    localStorage.setItem(
+      "list",
+      //JSON.stringify({ "list": thing }, null, 2)
+      JSON.stringify(
+        JSON.parse(localStorage.getItem("list")).concat([thing])
+      )
+    );
+  } else {
+    localStorage.setItem("list", JSON.stringify([].concat(thing), null, 2));
+  }
+};
 
-const Thing = ({ num, img, title, link, source, pos,type }) => {
+const Thing = ({ num, img, title, link, source, pos, type }) => {
   return (
-      <Card
-        shadow="sm"
-        p="lg"
-        radius="md"
-        withBorder
-        sx={{ width: "40vw", minWidth: "500px", margin: "50px", }}
-        className={pos}
-        key={num}
-      >
-        <Card.Section>
-          <Image src={img} height={"30vh"} alt={title} />
-        </Card.Section>
-        <Group position="apart" mt="md" mb="xs">
-          <Text weight={500}>{text_truncate(title,80)}</Text>
-          {/* CHANGE COLOR */}
-          <Badge color="" variant="light">
-            {source}
-          </Badge>
-        </Group>
-        <Group dir="row" grow>
-          <a href={link} target="_blank" rel="noreferrer">
-            <Button
-              variant="light"
-              color="blue"
-              mt="md"
-              radius="md"
-              sx={{ width: "100%", height: "40px" }}
-            >
-              <AiFillRead fontSize={"30px"} />
-            </Button>
-          </a>
+    <Card
+      shadow="sm"
+      p="lg"
+      radius="md"
+      withBorder
+      sx={{ width: "40vw", minWidth: "500px", margin: "50px" }}
+      className={pos}
+      key={num}
+    >
+      <Card.Section>
+        <Image src={img} height={"30vh"} alt={title} />
+      </Card.Section>
+      <Group position="apart" mt="md" mb="xs">
+        <Text weight={500}>{text_truncate(title, 80)}</Text>
+        {/* CHANGE COLOR */}
+        <Badge color="" variant="light">
+          {source}
+        </Badge>
+      </Group>
+      <Group dir="row" grow>
+        <a href={link} target="_blank" rel="noreferrer">
           <Button
+            variant="light"
+            color="blue"
+            mt="md"
+            radius="md"
+            sx={{ width: "100%", height: "40px" }}
+          >
+            <AiFillRead fontSize={"30px"} />
+          </Button>
+        </a>
+        <Button
           variant="light"
           color="green"
           mt="md"
           radius="md"
           sx={{ width: "20%", height: "40px" }}
-          onClick={() => { addToList(title, link, source, type) }}
-          >
-            <HiClipboardList fontSize={"30px"} />
-          </Button>
-        </Group>
-      </Card>
+          onClick={() => {
+            addToList(title, link, source, type);
+          }}
+        >
+          <HiClipboardList fontSize={"30px"} />
+        </Button>
+      </Group>
+    </Card>
   );
 };
 export default function Gallery({ rating }) {
   const [index, setIndex] = useState(0);
   const [gal, setGal] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+
   useEffect(
     function () {
       setGal(JSON.parse(localStorage.getItem("gal")));
@@ -106,13 +121,13 @@ export default function Gallery({ rating }) {
     }
   }, [index, gal]);
   //useEffect(() => {
-    //let slider = setInterval(() => {
-      //console.log(index);
-      //setIndex(index + 1);
-    //}, 3000);
-    //return () => {
-      //clearInterval(slider);
-    //};
+  //let slider = setInterval(() => {
+  //console.log(index);
+  //setIndex(index + 1);
+  //}, 3000);
+  //return () => {
+  //clearInterval(slider);
+  //};
   //}, [index]);
   return (
     <Box>
@@ -120,38 +135,45 @@ export default function Gallery({ rating }) {
         <Center sx={{ height: "80vh" }}>
           <Group spacing="md">
             <Box>
-              <AiFillLeftCircle fontSize={"40px"} onClick={()=>setIndex(index-1)} />
+              <AiFillLeftCircle
+                fontSize={"40px"}
+                onClick={() => setIndex(index - 1)}
+              />
             </Box>
             <Box className="section-center">
               <Center>
-            {gal.map((article, articleIndex) => {
-              const { title, img, url, source,type } = article;
-              let pos = "next";
-              if (articleIndex === index) {
-                pos = "active";
-              }
-              if (
-                articleIndex === index - 1 ||
-                (index === 0 && articleIndex === article.length - 1)
-              ) {
-                pos = "last";
-              }
-              return (
-                <Thing
-                  key={title}
-                  num={index}
-                  title={title}
-                  img={img}
-                  link={url}
-                  source={source}
-                  pos={pos}
-                  type={type}
-                />
-              );
-            })}</Center>
+                {gal.map((article, articleIndex) => {
+                  const { title, img, url, source, type } = article;
+                  let pos = "next";
+                  if (articleIndex === index) {
+                    pos = "active";
+                  }
+                  if (
+                    articleIndex === index - 1 ||
+                    (index === 0 && articleIndex === article.length - 1)
+                  ) {
+                    pos = "last";
+                  }
+                  return (
+                    <Thing
+                      key={title}
+                      num={index}
+                      title={title}
+                      img={img}
+                      link={url}
+                      source={source}
+                      pos={pos}
+                      type={type}
+                    />
+                  );
+                })}
+              </Center>
             </Box>
             <Box>
-              <AiFillRightCircle fontSize={"40px"} onClick={()=>setIndex(index+1)} />
+              <AiFillRightCircle
+                fontSize={"40px"}
+                onClick={() => setIndex(index + 1)}
+              />
             </Box>
           </Group>
         </Center>
